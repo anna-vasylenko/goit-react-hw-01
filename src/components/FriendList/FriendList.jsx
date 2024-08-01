fe144d7ec771ae02ab08aafa346bbf0805ec5e2b
anna-vasylenko <anna.bilych16@gmail.com>
2024-08-01 21:46:38 +0300

    update

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,15 @@
+import FriendListItem from "../FriendListItem/FriendListItem";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <li key={friend.id}>
+          <FriendListItem {...friend} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,11 @@
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+      <p>{isOnline ? "Online" : "Offline"}</p>
+    </div>
+  );
+};
+
+export default FriendListItem;
